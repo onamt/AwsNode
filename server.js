1ca538552e9c8324cb34ea254b07959f25766dba
onamt <onarkism@gmail.com>
2021-07-24 21:20:32 -0400

    texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Iniciando con express'))
+app.get('/', (req, res) => res.send('<h1>Express con HTML</h1>'))
 
 app.listen(3000);
 console.log('Iniciado');
